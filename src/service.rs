7575f65070dfce8b197f3a19d3c6cd078af27c34
Carl Lerche <me@carllerche.com>
2018-01-25 09:37:24 -0800

    Add lift combinators for HttpService. (#18)
    * Add lift combinator for HttpService.
    
    HttpService instances cannot be used where a `T: Service` is required.
    `LiftService` is provided to adapt an `HttpService` instance to
    `Service`.
    
    * Add lift_ref to HttpService.
    
    This is the same as `lift`, but operates on an HttpService reference.
    
    Unfortunately, due to coherence, &mut T: HttpService cannot directly
    implement either HttpService or Service. Thus, the reference must be
    lifted in order to use it when either a T: HttpService or T: Service is
    required.

--- src/service.rs
+++ src/service.rs
@@ -27,6 +27,39 @@ pub trait HttpService: ::sealed::Sealed {
 
     /// Process the request and return the response asynchronously.
     fn call(&mut self, request: Request<Self::RequestBody>) -> Self::Future;
+
+    /// Wrap the HttpService so that it implements tower::Service directly.
+    ///
+    /// Since `HttpService` does not directly implement `Service`, if an
+    /// `HttpService` instance needs to be used where a `T: Service` is
+    /// required, it must be wrapped with a type that provides that
+    /// implementation. `LiftService` does this.
+    fn lift(self) -> LiftService<Self> where Self: Sized {
+        LiftService { inner: self }
+    }
+
+    /// Same as `lift` but operates on an HttpService reference.
+    fn lift_ref(&mut self) -> LiftServiceRef<Self> where Self: Sized {
+        LiftServiceRef { inner: self }
+    }
+}
+
+/// Wraps an `HttpService` instance, implementing `tower::Service`.
+///
+/// See [`lift`] function documentation for more details.
+///
+/// [`lift`]: #
+pub struct LiftService<T> {
+    inner: T,
+}
+
+/// Wraps an `HttpService` reference, implementing `tower::Service`.
+///
+/// See [`lift`] function documentation for more details.
+///
+/// [`lift`]: #
+pub struct LiftServiceRef<'a, T: 'a> {
+    inner: &'a mut T,
 }
 
 impl<T, B1, B2> HttpService for T
@@ -55,3 +88,33 @@ where T: Service<Request = Request<B1>,
       B1: Body,
       B2: Body,
 {}
+
+impl<T: HttpService> Service for LiftService<T> {
+    type Request = Request<T::RequestBody>;
+    type Response = Response<T::ResponseBody>;
+    type Error = T::Error;
+    type Future = T::Future;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        self.inner.poll_ready()
+    }
+
+    fn call(&mut self, request: Self::Request) -> Self::Future {
+        self.inner.call(request)
+    }
+}
+
+impl<'a, T: HttpService> Service for LiftServiceRef<'a, T> {
+    type Request = Request<T::RequestBody>;
+    type Response = Response<T::ResponseBody>;
+    type Error = T::Error;
+    type Future = T::Future;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        self.inner.poll_ready()
+    }
+
+    fn call(&mut self, request: Self::Request) -> Self::Future {
+        self.inner.call(request)
+    }
+}
