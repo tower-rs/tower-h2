f290b7f1a51ff2f4322d012feac7fceea0144b69
Eliza Weisman <eliza@buoyant.io>
2018-09-28 14:51:42 -0700

    add trace example

--- examples/server.rs
+++ examples/server.rs
@@ -1,11 +1,11 @@
 extern crate bytes;
-extern crate env_logger;
 extern crate futures;
 extern crate h2;
 extern crate http;
-#[macro_use]
-extern crate log;
 extern crate tokio;
+#[macro_use]
+extern crate tokio_trace;
+extern crate tokio_trace_tower_http;
 extern crate tower_h2;
 extern crate tower_service;
 
@@ -14,8 +14,16 @@ use futures::*;
 use http::Request;
 use tokio::net::TcpListener;
 use tokio::runtime::Runtime;
+use tokio_trace::{
+    Level,
+    instrument::Instrument,
+};
 use tower_h2::{Body, Server, RecvBody};
 use tower_service::{NewService, Service};
+
+#[path = "../../tokio-trace/examples/sloggish/sloggish_subscriber.rs"]
+mod sloggish;
+use self::sloggish::SloggishSubscriber;
 
 type Response = http::Response<RspBody>;
 
@@ -62,6 +70,7 @@ impl Service for Svc {
     }
 
     fn call(&mut self, req: Self::Request) -> Self::Future {
+        event!(Level::Debug, {}, "received request");
         let mut rsp = http::Response::builder();
         rsp.version(http::Version::HTTP_2);
 
@@ -69,6 +78,7 @@ impl Service for Svc {
         if uri.path() != ROOT {
             let body = RspBody::empty();
             let rsp = rsp.status(404).body(body).unwrap();
+            event!(Level::Warn, { status_code = 404, path = uri.path() }, "unrecognized URI");
             return future::ok(rsp);
         }
 
@@ -88,38 +98,54 @@ impl NewService for NewSvc {
     type Service = Svc;
     type Future = future::FutureResult<Svc, Self::InitError>;
 
-    fn new_service(&self) -> Self::Future {
+   fn new_service(&self) -> Self::Future {
         future::ok(Svc)
     }
 }
 
 fn main() {
-    drop(env_logger::init());
+    tokio_trace::Dispatcher::builder()
+        .add_subscriber(SloggishSubscriber::new(2))
+        .init();
 
     let mut rt = Runtime::new().unwrap();
     let reactor = rt.executor();
 
-    let h2 = Server::new(NewSvc, Default::default(), reactor.clone());
-
     let addr = "[::1]:8888".parse().unwrap();
     let bind = TcpListener::bind(&addr).expect("bind");
 
-    let serve = bind.incoming()
-        .fold((h2, reactor), |(h2, reactor), sock| {
-            if let Err(e) = sock.set_nodelay(true) {
-                return Err(e);
-            }
+    let server_span = span!("serve", local_ip = addr.ip(), local_port = addr.port());
+    server_span.clone().enter(move || {
+        let new_svc = tokio_trace_tower_http::InstrumentedNewService::new(NewSvc);
+        let h2 = Server::new(new_svc, Default::default(), reactor.clone());
 
-            let serve = h2.serve(sock);
-            reactor.spawn(serve.map_err(|e| error!("h2 error: {:?}", e)));
+        let serve = bind.incoming()
+            .fold((h2, reactor), |(h2, reactor), sock| {
+                let addr = sock.peer_addr().expect("can't get addr");
+                let conn_span = span!("conn", remote_ip = addr.ip(), remote_port = addr.port());
+                conn_span.clone().enter(|| {
+                    if let Err(e) = sock.set_nodelay(true) {
+                        return Err(e);
+                    }
 
-            Ok((h2, reactor))
-        })
-        .map_err(|e| error!("serve error: {:?}", e))
-        .map(|_| {})
-        ;
+                    event!(Level::Info, {}, "accepted connection");
 
-    rt.spawn(serve);
-    rt.shutdown_on_idle()
-        .wait().unwrap();
+                    let serve = h2.serve(sock)
+                        .map_err(|e| event!(Level::Error, { }, "error {:?}", e))
+                        .map(|_| event!(Level::Debug, { }, "response finished"))
+                        .instrument(conn_span);
+                    reactor.spawn(Box::new(serve));
+
+                    Ok((h2, reactor))
+                })
+
+            })
+            .map_err(|e| event!(Level::Error, {}, "serve error {:?}", e))
+            .map(|_| {})
+            ;
+
+        rt.spawn(serve);
+        rt.shutdown_on_idle()
+            .wait().unwrap();
+    })
 }
